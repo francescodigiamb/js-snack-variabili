b348c29fd0ee6498660881f8b53a640103989cd8
francescodgiamb <francescodigiamb@gmail.com>
2024-06-30 12:00:17 +0200

    ADD:complete

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// Esercizio 1
+// Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+
+let number = 100; //number
+console.log (number)
+
+// Esercizio 2
+// Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console
+
+let PI = 3.14; //number
+console.log (PI)
+
+// Esercizio 2/B
+// Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+
+PI = 3.1416 //number
+console.log (PI)
+
+// Esercizio 2/C 
+// Dichiara una variabile chiamata radius e assegnale il valore 8.
+
+let radius = 8; //number
+
+// Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+// Stampa il valore della variabile circle in console. 
+
+let circle;
+circle = radius * 2 * PI //number
+console.log (circle)
+
+// Esercizio 3
+// Dichiara una variabile chiamata name e assegnale il valore 'Carlo'
+
+let name = 'Carlo'; // stringa
+console.log (name)
+
+// modifica il valore della variabile in Marco
+name='Marco';
+// stampa il nuovo valore in console
+console.log (name)
+
+
